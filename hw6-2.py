0b7b102423b9532c31e72aadc04840accdcf34a2
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-11-16 08:17:40 -0500

    Create hw6-2.py
    add hw6-2

---
+++ hw6-2.py
@@ -0,0 +1,26 @@
+#Author:JTI 11/9/21
+
+#Question 1
+my_lst = [0, 1, 2, 3, 4, 5, 6, 7]
+print(my_lst)
+
+#Question 2
+print(len(my_lst))
+
+#Question 3
+print(my_lst[3])
+
+#Question 4
+print([5] in (my_lst))
+
+#Question 5
+print([10] not in (my_lst))
+
+#Question 6
+names = ['Charlie', 'Jack', 'Nolan', 'Rayn']
+
+#Question 7
+#print(names.index[3])
+
+#Question 8
+print(names ** 3)
